d4dcf1fd40e6d179227d28a6911181cc4c1582b2
Omar <omoral02@live.com>
2021-03-08 03:44:24 -0600

    adjust README for orientation

---
+++ code_challenges/haystack.java
@@ -0,0 +1,24 @@
+package code_challenges;
+
+public class haystack {
+
+    public int threadedNeedle(String haystack, String needle) {
+        String bundle = haystack;
+        String filter = needle;
+        int h = bundle.length();
+        int n = filter.length();
+        if (n == 0) {
+            return 0;
+        }
+        if (n > h) {
+            return -1;
+        }
+
+        for (int i = 0; i < h - n + 1; i++) {
+            if (bundle.substring(i, i + n).equals(filter)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
