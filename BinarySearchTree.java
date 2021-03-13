b2a74e94a5eb5ba8e97eda0727e04eea9c90aaf1
Omar <omoral02@live.com>
2021-03-13 15:37:28 -0600

    new struct

---
+++ BinarySearchTree.java
@@ -0,0 +1,24 @@
+package data_structs;
+
+public class BinarySearchTree {
+    private class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    public boolean checkBST(Node root, int min, int max){
+        if (root == null){
+            return true;
+        } 
+        if (root.data < min || root.data > max){
+            return false;
+        }
+        return checkBST(root.)
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
