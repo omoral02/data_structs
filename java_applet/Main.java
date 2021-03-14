ae1d270a7190c9c5d639be9e530c78d06d5b0551
Omar <omoral02@live.com>
2021-03-14 03:18:41 -0500

    add java applet defaults

--- java_applet/Main.java
+++ java_applet/Main.java
@@ -2,7 +2,37 @@ package java_applet;
 
 import java.applet.*;
 import java.awt.*;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main extends Applet implements Runnable {
+    Thread thread = new Thread(this);
+    boolean running = true;
+
+    public void init() {
+    }
+
+    public void start() {
+        thread.start();
+    }
+
+    public void destroy() {
+        running = false;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public void run() {
+        while (running) {
+            try {
+
+            } catch (InterruptedException e) {
+                System.out.println("Error has occured:" + e);
+                // nothing happens here or else print error
+            }
+        }
+    }
 
 }
