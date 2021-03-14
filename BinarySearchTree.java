996666046690a1e99cfb70927d95008385144560
Omar <omoral02@live.com>
2021-03-14 03:09:01 -0500

    minor changes

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -7,14 +7,14 @@ public class BinarySearchTree {
         Node right;
     }
 
-    public boolean checkBST(Node root, int min, int max){
-        if (root == null){
+    public boolean checkBST(Node root, int min, int max) {
+        if (root == null) {
             return true;
-        } 
-        if (root.data < min || root.data > max){
+        }
+        if (root.data < min || root.data > max) {
             return false;
         }
-        return checkBST(root.)
+        return checkBST(root, min, max);
     }
 
     public static void main(String[] args) {
